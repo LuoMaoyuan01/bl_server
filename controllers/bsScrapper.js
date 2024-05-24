3a28423bdabb86f6357d9d6564c18353ce4d9415
luohr <luohr180@outlook.com>
2024-05-24 17:03:39 +0800

    Add cheerio object

--- controllers/bsScrapper.js
+++ controllers/bsScrapper.js
@@ -8,11 +8,20 @@ const getBusStops = async(req, res) => {
     const baseURL = `https://www.sbstransit.com.sg/Service/BusService?ServiceType=Basic&ServiceNo=${busNumber}`;
     try {
         axios.get(baseURL).then((response) => {
+
+            // Loads HTML content obtained and parses it as a Cheerio object to be used to manipulate HTML data
+            // $ variable assigned to Cheerio object, mimicking jQuery usage in the browser. Allows usage of jQuery style selectors & methods
+            const $ = cheerio.load(response.data);
+
+            // Create an array to store desired data
+            const busStops = [];
+                
             res.status(200).json(response.data);
         })
     }
     catch (error){
         console.error(error);
+        // Returns an error in JSON format
         res.status(500).json({ error: 'An error occurred while fetching bus stops' });
     }
 };  
