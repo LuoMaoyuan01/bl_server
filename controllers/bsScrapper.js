5cc8fd5993c65d391cc709e5c4da805dd6963de7
luohr <luohr180@outlook.com>
2024-05-31 17:25:03 +0800

    Added a API KEY and deleted some stuff

--- controllers/bsScrapper.js
+++ controllers/bsScrapper.js
@@ -9,7 +9,6 @@ const getBusStops = async(req, res) => {
     const busNumber = req.params.busNumber;
     const dir = req.params.direction - 1;
     const baseURLdir = `https://www.lta.gov.sg/map/busService/bus_route_xml/${busNumber.toString()}.xml`;
-    console.log(baseURLdir);
 
     try {
         // Create an array to store desired data
@@ -23,13 +22,7 @@ const getBusStops = async(req, res) => {
             
             // Returns the table needed in the DOM
             const busStopTableBody = $(`route direction:eq(${dir}) busstop`);
-             
-            // Debugging purposes
-            // console.log(busStopTableBody.text());
-            // busStopTableBody.each((_idx, el) => {
-            //     console.log($(el).html());
-            // });
-            // console.log($.text());
+
             
             // Loops over each tr in the tbody and obtains the neccessary data from each tr
             busStopTableBody.each((_idx, el) => {
@@ -47,56 +40,7 @@ const getBusStops = async(req, res) => {
                 busStops.push(busData); 
 
             });
-
-
-            // Iterate through 'roadName' field and check if next row's 'roadName' field is empty
-            // If empty, set to current 'roadName' field value
-            // for(let i = 0; i < busStops.length; i++){
-            //     if(i+1 == busStops.length){
-            //         break;
-            //     }
-            //     if(busStops[i+1]['Road Name'] == ''){
-            //         busStops[i+1]['Road Name'] = busStops[i]['Road Name'];
-            //     }
-            //     else{
-            //         continue;
-            //     }
-            // }
-
         })
-
-        // Need a check to see if the last busData contains the final stop (eg: bus 70 has wonky data)
-        // If check fails, go to dir2 and take their first data to append to busStops
-        // if(dir == 1){
-        //     const baseURLdir = `https://www.sbstransit.com.sg/Service/BusService?ServiceType=Basic&ServiceNo=${busNumber}&ServiceType=Basic&Dir=2`;
-        //     await axios.get(baseURLdir).then((response) => {
-        //         const $ = cheerio.load(response.data);
-        //         const busStopTableBodyAmended = $('table.tbres.lefttext.tb-fix tbody tr:eq(1)');
-
-        //         const roadName = $(busStopTableBodyAmended).find('td.normal-line.fontbold').text().substring(0, 35).trim();
-        //         const busStopNo = $(busStopTableBodyAmended).find('td.col1bg.center-text').text().trim();
-        //         const busStopName = $(busStopTableBodyAmended).find('td.col2bg.fontbold').text().trim().substring(0, 25).trim();
-
-        //         const busData = {
-        //             'Road Name' : roadName,
-        //             'Bus Stop Number' : busStopNo,
-        //             'Bus Stop Name' : busStopName
-        //         };
-
-        //         // Check if last item in busStops list corresponds to first item in the second bus path
-        //         if(busStops[busStops.length - 1]['Bus Stop Number'] != busData['Bus Stop Number']){
-        //             busStops.push(busData);
-        //         }
-        //     })
-
-        // }
-
-        // Iterate through list of arrays and create new key 'Road + Bus Stop' which joins road name to bus stop name for more accurate geolocation
-        // for(let j = 0; j<busStops.length; j++){
-        //     // Utilize regex to replace all whitespaces within the string to '+'
-        //     // Concatenate 2 array fields into a new fi eld
-        //     busStops[j]['Bus Stop + Road'] = (busStops[j]['Bus Stop Name'] + '+' + busStops[j]['Road Name']).replace(/\s/g, "+");
-        // }
 
         // Obtain lta.gov.sg xml file on bus stop lat & lng
         // Append latlng of busstop corresponding to the correct bus stop number
